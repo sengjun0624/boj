06a3f31fba88514c292bd8c509120f1dbcf87954
이승준 <105282117+sengjun0624@users.noreply.github.com>
2024-04-15 00:04:50 +0900

    [Silver IV] Title: 대칭 차집합, Time: 236 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int arr1[200001];
+int arr2[200001];
+
+int A, B;
+
+int main() {
+	cin >> A >> B;
+
+	for (int i = 0; i < A;i++) {
+		cin >> arr1[i];
+	}
+	for (int i = 0; i < B; i++) {
+		cin >> arr2[i];
+	}
+	sort(arr1, arr1+A);
+	sort(arr2, arr2 + B);
+
+	int cnt = 0;
+	int ans = 0;
+	for (int i = 0; i < A; i++) {
+		int st = 0; int ed = B-1;
+		while (st <= ed) {
+			int mid = (st + ed) / 2;
+			if (arr2[mid] == arr1[i]) {
+				cnt ++; 
+				break;
+			}
+			else if (arr2[mid] < arr1[i]) {
+				st = mid+1;
+			}
+			else {
+				ed= mid -1;
+			}
+		}
+	}
+	ans += A - cnt;
+	cnt = 0;
+	for (int i = 0; i < B; i++) {
+		int st = 0; int ed = A - 1;
+		while (st <= ed) {
+			int mid = (st + ed) / 2;
+			if (arr1[mid] == arr2[i]) {
+				cnt++;
+				break;
+			}
+			else if (arr1[mid] < arr2[i]) {
+				st = mid + 1;
+
+			}
+			else {
+				ed = mid - 1;
+			}
+		}
+	}
+	ans += B - cnt;
+	cnt = 0;
+	cout << ans;
+
+}
